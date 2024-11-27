a7d5087d1c1a540a722c10dea463325c3203fbda
比乐喵 <17762591607@163.com>
2024-11-27 17:08:20 +0800

    修复弹簧不会反向弹的bug，但是产生了新的问题

--- Scenes/GameItems/Spring/spring.gd
+++ Scenes/GameItems/Spring/spring.gd
@@ -6,4 +6,5 @@ func _ready() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		body.velocity = Vector2(100,-400).rotated(deg_to_rad(10))
+		#body.velocity = Vector2(100,-400).rotated(deg_to_rad(10))
+		body.velocity = Vector2(100* body.go_back,-400)
